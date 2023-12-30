ce7c63117b57ff04dc9b4ba84a5c06c2cde45f74
seba-sanchis <seba.sanchis@gmail.com>
2023-12-30 20:44:39 -0300

    testing routes

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterModule } from '@angular/router';
+import { Router, NavigationEnd, RouterModule, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
